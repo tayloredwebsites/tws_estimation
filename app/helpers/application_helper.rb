e49781e43623de2245a139eb0e77caabfcf8e9f6
Dave Taylor <tayloredwebsites@me.com>
2011-08-03 19:26:45 -0400

    TDD for Home controller Views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
 
-    # title method to place title from page into layout file
+    # page title method to place title from page into layout file
     def page_title (pg_title)
         content_for(:page_title) { pg_title }
     end
