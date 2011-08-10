e7b58a16fbe2f4cefc0d22a35353b1d8823d5306
Dave Taylor <tayloredwebsites@me.com>
2011-08-10 11:51:08 -0400

    added a _spec_to_do.rb file with major items, but don't want it pending yet. adjusted autotest to not include _to_do.rb files.

---
+++ spec/integration/integration_spec_to_do.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+require 'capybara_spec_helper'
+
+describe 'Integration Tests To Do- ' do
+
+	companyName = APP_CONFIG['COMPANY_NAME']
+	appName = APP_CONFIG['APP_NAME']
+	homeIndexTitle = I18n.translate('home.index.title')
+	homeIndexHeader = I18n.translate('home.index.header')
+
+	context 'visit root_path/home_index/Home#index page - ' do
+		before(:each) do
+			visit home_index_path
+		end
+		it 'should have app_config in ruby hash'
+		it 'should have app_config internationalized'
+		it 'should have an internationalization application helper'
+		it 'should be html5 with graceful degredations'
+		it 'should have user authentication'
+		it 'should have role based authorization'
+		it 'should have application specified in roles'
+		
+	end
+
+end
