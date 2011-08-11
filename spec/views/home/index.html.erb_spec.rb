24dc073be8b04f3076ca3d67aee00ecc8d9c521e
Dave Taylor <tayloredwebsites@me.com>
2011-08-10 20:17:42 -0400

    tabs to spaces now consistent in all files

--- spec/views/home/index.html.erb_spec.rb
+++ spec/views/home/index.html.erb_spec.rb
@@ -3,34 +3,34 @@ require 'capybara_spec_helper'
 
 describe "home/index.html.erb" do
 
-	appName = APP_CONFIG['APP_NAME']
-	companyName = APP_CONFIG['COMPANY_NAME']
-	pageTitle = I18n.translate('home.index.title')
-	pageHeader = I18n.translate('home.index.header')
+  appName = APP_CONFIG['APP_NAME']
+  companyName = APP_CONFIG['COMPANY_NAME']
+  pageTitle = I18n.translate('home.index.title')
+  pageHeader = I18n.translate('home.index.header')
 
-	before(:each) do
-		render
-	end
-	describe "Home Index view test - " do
-		context "page content - " do
-			it "should find the header tag content" do
-				rendered.should have_selector('h4', :text => pageHeader)
-			end
-			it "should find a span selector with content" do
-				rendered.should have_selector('div.field[1]/span.label', :text => 'Environment')
-			end
-			it "should find a span selector with content" do
-				rendered.should have_selector('div.field[1]/span.value', :text => ::Rails.env)
-			end
-			it "should find a span selector with content" do
-				rendered.should have_selector('div.field[3]/span.label', :text => 'APP_NAME')
-			end
-			it "should not find a span selector with garbage" do
-				rendered.should have_selector('div.field[3]/span.value', :text => appName)
-			end
-			it "should not find a span selector with garbage" do
-				rendered.should_not have_selector('div.field[3]/span.value', :text => 'xxxx')
-			end
-		end
-	end
+  before(:each) do
+    render
+  end
+  describe "Home Index view test - " do
+    context "page content - " do
+      it "should find the header tag content" do
+        rendered.should have_selector('h4', :text => pageHeader)
+      end
+      it "should find a span selector with content" do
+        rendered.should have_selector('div.field[1]/span.label', :text => 'Environment')
+      end
+      it "should find a span selector with content" do
+        rendered.should have_selector('div.field[1]/span.value', :text => ::Rails.env)
+      end
+      it "should find a span selector with content" do
+        rendered.should have_selector('div.field[3]/span.label', :text => 'APP_NAME')
+      end
+      it "should not find a span selector with garbage" do
+        rendered.should have_selector('div.field[3]/span.value', :text => appName)
+      end
+      it "should not find a span selector with garbage" do
+        rendered.should_not have_selector('div.field[3]/span.value', :text => 'xxxx')
+      end
+    end
+  end
 end
--- spec/views/home/sample_help.html.erb_spec.rb
+++ spec/views/home/sample_help.html.erb_spec.rb
@@ -3,40 +3,40 @@ require 'capybara_spec_helper'
 
 describe "home/help.html.erb" do
 
-	appName = APP_CONFIG['APP_NAME']
-	companyName = APP_CONFIG['COMPANY_NAME']
-	pageTitle = I18n.translate('home.help.title')
-	pageHeader = I18n.translate('home.help.header')
+  appName = APP_CONFIG['APP_NAME']
+  companyName = APP_CONFIG['COMPANY_NAME']
+  pageTitle = I18n.translate('home.help.title')
+  pageHeader = I18n.translate('home.help.header')
 
-	before(:each) do
-		render
-	end
-	describe "Home Index view test - " do
-		context "page content - " do
-			it "should find the header tag content" do
-				rendered.should have_selector('h4')
-			end
-			it "should find the header tag content" do
-				rendered.should have_selector('h4', :text => pageHeader)
-			end
-			it "should find a span selector with content" do
-				rendered.should have_selector('div.field[1]/span.label', :text => 'To Do')
-			end
-			it "should find via xpath a span selector with content" do
-				rendered.should have_selector(:xpath, '//div[@class="field"][1]/span[@class="label"]', :text => 'To Do')
-			end
-			it "should find a span selector with content" do
-				rendered.should have_selector('span', :text => 'Update me in app/views/home/help.html.erb')
-			end
-			it "should find a span selector with content" do
-				rendered.should have_selector('div.field[1]/span.value', :text => 'Update me in app/views/home/help.html.erb')
-			end
-			it "should not find a span selector with garbage" do
-				rendered.should_not have_selector('span', :text => 'xxxx')
-			end
-			it "should not find a span selector with garbage" do
-				rendered.should_not have_selector('div.field[1]/span.value', :text => 'xxxx')
-			end
-		end
-	end
+  before(:each) do
+    render
+  end
+  describe "Home Index view test - " do
+    context "page content - " do
+      it "should find the header tag content" do
+        rendered.should have_selector('h4')
+      end
+      it "should find the header tag content" do
+        rendered.should have_selector('h4', :text => pageHeader)
+      end
+      it "should find a span selector with content" do
+        rendered.should have_selector('div.field[1]/span.label', :text => 'To Do')
+      end
+      it "should find via xpath a span selector with content" do
+        rendered.should have_selector(:xpath, '//div[@class="field"][1]/span[@class="label"]', :text => 'To Do')
+      end
+      it "should find a span selector with content" do
+        rendered.should have_selector('span', :text => 'Update me in app/views/home/help.html.erb')
+      end
+      it "should find a span selector with content" do
+        rendered.should have_selector('div.field[1]/span.value', :text => 'Update me in app/views/home/help.html.erb')
+      end
+      it "should not find a span selector with garbage" do
+        rendered.should_not have_selector('span', :text => 'xxxx')
+      end
+      it "should not find a span selector with garbage" do
+        rendered.should_not have_selector('div.field[1]/span.value', :text => 'xxxx')
+      end
+    end
+  end
 end
