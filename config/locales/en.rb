7b249108b03881f7c7e7a91b767918cbaf28369d
Dave Taylor <tayloredwebsites@me.com>
2011-08-10 20:17:42 -0400

    tabs to spaces now consistent in all files

--- config/locales/en.rb
+++ config/locales/en.rb
@@ -1,15 +1,16 @@
+# config/locales/en.rb
 # localization file using ruby hash method (instead of yaml).
 {
-	:en => {
-		:home => {
-			:index => {
-				:title => "Home",
-				:header => "Application Configuration Constants"
-			},
-			:help => {
-				:title => "Help",
-				:header => "Help Pages"
-			}
-		}		# end home
-	}		# end en
+  :en => {
+    :home => {
+      :index => {
+        :title => "Home",
+        :header => "Application Configuration Constants"
+      },
+      :help => {
+        :title => "Help",
+        :header => "Help Pages"
+      }
+      }		# end home
+  }		# end en
 }		#end localizations
