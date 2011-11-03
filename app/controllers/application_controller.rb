409974be234eea3bb201c90a2d1a501a57b1629c
Dave Taylor <tayloredwebsites@me.com>
2011-11-02 22:33:40 -0400

    fixes from update to heroku

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  require 'capybara'
   
   include ApplicationHelper
   
--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -13,16 +14,16 @@
 ActiveRecord::Schema.define(:version => 20110919153230) do
 
   create_table "users", :force => true do |t|
-    t.string   "first_name"
-    t.string   "last_name"
-    t.string   "email"
-    t.string   "roles"
-    t.string   "username"
-    t.string   "encrypted_password"
-    t.string   "password_salt"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.boolean  "deactivated"
+    t.string    "first_name"
+    t.string    "last_name"
+    t.string    "email"
+    t.string    "roles"
+    t.string    "username"
+    t.string    "encrypted_password"
+    t.string    "password_salt"
+    t.timestamp "created_at"
+    t.timestamp "updated_at"
+    t.string    "deactivated"
   end
 
 end
