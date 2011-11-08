241e1d031d96bdb5a5a19a6189ccf20bbcff2568
Dave Taylor <tayloredwebsites@me.com>
2011-11-08 18:42:03 -0500

    unique username and email addresses are required for users

--- spec_to_do/to_do_integration_spec.rb
+++ spec_to_do/to_do_integration_spec.rb
@@ -3,36 +3,24 @@ require 'capybara_spec_helper'
 
 describe 'Integration Tests To Do- ' do
 
-  context 'User Authentication' do
-    it 'may need to use email validations see lesson 56, 56:00 - 1:20:00'
-    context 'all users -' do
-      it 'should be able to navigate to the PUT reset_password page'
-    end
-    context 'not logged in (guest user) -' do
-    end
-    context 'logged in user -' do
-      it 'should be able to update password'
-    end
-
-  end
-  
-
   context 'User Role Based Authorization' do
     it 'should have role based authorization'
     it 'should have application specified in roles'
     it 'should have multiple applications allowed in the app_config'
+    it 'should allow (administrative) users to assign a user to a role'
+    it 'should allow (administrative) users to assign a user to an application with a particular role'
+    it 'should allow (administrative) users to assign a user to all applications with a particular'
     it 'should have applications to have their own scope'
   end
 
-  context 'User Administration' do
+  context 'User Administration - ' do
     it 'should have an admin namespaced controller for User Maintenance'
-    it 'should remove the ability for any user to create a user'
-    it 'should only allow users with the admin role (for all applications?) to access the user maintenance controller (and actions)'
+    it 'should not allow a regular user (per application) to access the user maintenance controller (and actions)'
+    it 'should not allow a regular user to assign a user to a role'
+    it 'should allow users with the admin role (per application) to create a user'
+    it 'should allow users with the admin role (per application) to access the user maintenance controller (and actions)'
+    it 'should not allow a regular user to create a user'
     it 'should list all users (by application or all?)'
-    it 'should allow crud access to users (by application or all?)'
-    it 'should allow users to be assigned to a role'
-    it 'should allow users to be assigned to an application with a particular role'
-    it 'should allow users to be assigned a role to all applications'
   end
 
   context 'Miscellaneous items to do' do
@@ -42,6 +30,8 @@ describe 'Integration Tests To Do- ' do
         it 'should have a convenient scoping mechanism for controllers, actions and pages'
       end
     end
+    it 'should ensure that username comes from name in email address (less @domain)'
+    it 'should send user their reset password'
     it 'should have standardized yields with the layout'
     it 'should be html5 with graceful degradations'
   end
