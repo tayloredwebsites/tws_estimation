734c587bbbde89a672394ff9b03181648534ac96
Dave Taylor <tayloredwebsites@me.com>
2012-02-23 18:34:00 -0500

    Converted to PostgreSQL 9 (per heroku strong recommendation)

--- app/models/user.rb
+++ app/models/user.rb
@@ -293,6 +293,7 @@ class User < ActiveRecord::Base
 end
 
 
+
 # == Schema Information
 #
 # Table name: users
@@ -305,8 +306,8 @@ end
 #  username           :string(255)
 #  encrypted_password :string(255)
 #  password_salt      :string(255)
-#  created_at         :datetime
-#  updated_at         :datetime
+#  created_at         :datetime        not null
+#  updated_at         :datetime        not null
 #  deactivated        :boolean
 #
 
