94193046a95be9e68a4671245c17e24850e0b392
David Taylor <tayloredwebsites@davids-imac.westell.com>
2012-03-24 12:13:42 -0400

    customize generated scaffold erbs

---
+++ lib/generators/tws_views/tws_views_generator.rb
@@ -0,0 +1,123 @@
+# lib/generators/tws_views/tws_views_generator.rb
+
+# see:
+# http://edgeguides.rubyonrails.org/generators.html
+# http://asciicasts.com/episodes/218-making-generators-in-rails-3
+# http://rdoc.info/github/wycats/thor/master/Thor/Actions
+
+require 'rails/generators/resource_helpers'
+
+
+class TwsViewsGenerator < Rails::Generators::NamedBase
+  include Rails::Generators::ResourceHelpers
+
+  source_root File.expand_path('../templates', __FILE__)
+  argument :view_name, :type => :string, :default => "all"
+  argument :attributes, :type => :array, :default => [], :banner => "field:type field:type"
+  # argument :model_attributes, type: :array, default: [], banner: "model:attributes"
+  
+  # argument :attributes, :type => :array, :default => ['description:string', 'calc_only:boolean', 'deactivated:boolean', 'component_type_id:integer', 'default_id:integer'], :banner => "field:type field:type"
+  # argument :attributes, :type => :array, :default => public_get_attributes, :banner => "field:type field:type"
+
+  
+  def initialize(*args, &block)
+    # load up attributes from model automatically, if not supplied
+    # todo - detect attributes that are not accessible, and either not output them, or tell generator to show only
+    # -> http://stackoverflow.com/questions/6285312/how-to-get-at-generatedattribute-in-a-custom-controller-generator
+    super
+    @attributes = []
+    if attributes.size > 0
+      Rails.logger.debug("G attributes: #{attributes.inspect.to_s}")
+      attributes.each do |attribute|
+        @attributes << Rails::Generators::GeneratedAttribute.new(*attribute.split(":")) if attribute.include?(":")
+      end
+    else
+      # Rails.logger.debug("G Load attributes from #{file_name.capitalize.constantize.columns.map.inspect.to_s}")
+      attributes = file_name.capitalize.constantize.columns.map do |c|
+        attrib = Rails::Generators::GeneratedAttribute.new(c.name, c.type)
+        # attrib_pair = "#{c.name}:#{c.type}"
+        Rails.logger.debug("G attrib = #{attrib.inspect.to_s}")
+        @attributes << attrib
+      end
+    end
+  end
+  
+  
+  
+  def copy_files
+    if view_name == "all"
+      # loop through all files in templates directory
+      Dir.foreach(get_first_source_path) do |filename|
+        if File.directory?(filename)
+          # Rails.logger.debug( "Ignore sub-directories" )
+        else
+          clean_filename = Pathname.new(filename).basename.to_s
+          Rails.logger.debug( "Got filename: #{clean_filename}" )
+          process_file(clean_filename)
+        end
+      end
+    else
+      # generate a template for the view_name file
+      # copy_file "#{view_name.underscore}.html.erb", "app/views/"
+      Rails.logger.debug( "Checking view_name: #{get_first_source_path.to_s+'/'+view_name}" )
+      if File.exists?(get_first_source_path+'/'+view_name)  # 
+        Rails.logger.debug( "Got view_name: #{view_name.inspect.to_s}" )
+        process_file(view_name)
+      end
+    end
+    
+  end
+  
+  # def list_source_paths
+  #   source_paths.each do |source|
+  #     Rails.logger.debug( "Possible Source Path = #{source.inspect.to_s}" )
+  #     source_file = File.expand_path(File.join(source, '.'))
+  #     if File.exists?(source_file)
+  #       my_source_abs_path = Pathname.new(source_file)
+  #       Rails.logger.debug( "my_source_abs_path = #{my_source_abs_path}" )
+  #     end
+  #   end
+  # end
+
+  # private methods only to be called by public methods
+  private
+  
+  def get_first_source_path
+    source_paths.each do |source|
+      source_file = File.expand_path(File.join(source, '.'))
+      return source_file if File.exists?(source_file)
+    end
+  end
+  
+  def get_first_source_rel_path
+    source_paths.each do |source|
+      source_file = File.expand_path(File.join(source, '.'))
+      if File.exists?(source_file)
+        my_base_path = Pathname.new(File.expand_path('.'))
+        # Rails.logger.debug("my_base_path = #{my_base_path}")
+        my_source_abs_path = Pathname.new(source_file)
+        # Rails.logger.debug( "my_source_abs_path = #{my_source_abs_path}" )
+        return Pathname.new(source_file).relative_path_from(my_base_path)
+      end
+    end
+  end
+  
+  def get_rel_destination_path
+    return "app/views/#{plural_name}"
+  end
+  
+  def process_file(filename)
+    Rails.logger.debug("G Process file: #{filename}")
+    if filename.to_s.match('.*erb$')
+      Rails.logger.debug( "Is an erb - #{get_rel_destination_path}/#{filename}" )
+      template filename, "#{get_rel_destination_path}/#{filename}"
+    elsif view_name.to_s.match('.*\.rb$')
+      Rails.logger.debug( "ruby file" )
+      copy_file filename, "#{get_rel_destination_path}/#{filename}"
+      # template filename, "#{get_rel_destination_path}/#{filename}"
+    else
+      Rails.logger.debug( "regular file" )
+    end
+  end
+
+end
