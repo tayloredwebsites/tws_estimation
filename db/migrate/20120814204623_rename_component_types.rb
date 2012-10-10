1828402b26fe7c3449c5134e8d918cf79051230c
Taylored Web Sites <tayloredwebsites@me.com>
2012-10-10 10:49:30 -0400

    removed extra renaming of component type field in_totals_grid

--- db/migrate/20120814204623_rename_component_types.rb
+++ db/migrate/20120814204623_rename_component_types.rb
@@ -1,9 +1,9 @@
 class RenameComponentTypes < ActiveRecord::Migration
   def up
-    rename_column :component_types, :no_entry, :totals_grid
+    rename_column :component_types, :no_entry, :in_totals_grid
   end
 
   def down
-    rename_column :component_types, :totals_grid, :no_entry
+    rename_column :component_types, :in_totals_grid, :no_entry
   end
 end
