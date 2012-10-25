87abf9bf9baa1eb6af2ee8caaeba59e5015f0ac0
Dave Taylor <tayloredwebsites@me.com>
2012-10-25 17:56:47 -0400

    have Legacy Estimation database basically match SQL server (so production and dev use same conversion code)

--- extras/legacy/legacy_classes.rb
+++ extras/legacy/legacy_classes.rb
@@ -10,7 +10,7 @@ module Legacy::LegacyClasses
   class LegacyComponentType < ActiveRecord::Base
     establish_connection :legacy
     # self.table_name = 'component_types'
-    self.table_name = 'ComponentTypes'
+    self.table_name = 'componenttypes'
     # set primary_key to prevent "Unknown bind columns. We can account for this." in sqlserver adapter
     self.primary_key = 'comp_type_id'
   end
@@ -23,21 +23,21 @@ module Legacy::LegacyClasses
   class LegacyAssembly < ActiveRecord::Base
     establish_connection :legacy
     # self.table_name = 'assemblies'
-    self.table_name = 'Systems'
+    self.table_name = 'systems'
     # set primary_key to prevent "Unknown bind columns. We can account for this." in sqlserver adapter
     self.primary_key = 'sys_id'
   end
   class LegacyAssemblyComponent < ActiveRecord::Base
     establish_connection :legacy
     # self.table_name = 'assembly_components'
-    self.table_name = 'SystemComponents'
+    self.table_name = 'systemcomponents'
     # set primary_key to prevent "Unknown bind columns. We can account for this." in sqlserver adapter
     self.primary_key = 'sys_comp_id'
   end
   class LegacyJobType < ActiveRecord::Base
     establish_connection :legacy
     # self.table_name = 'job_types'
-    self.table_name = 'TaxTypes'
+    self.table_name = 'taxtypes'
     # set primary_key to prevent "Unknown bind columns. We can account for this." in sqlserver adapter
     self.primary_key = 'tax_type_id'
   end
